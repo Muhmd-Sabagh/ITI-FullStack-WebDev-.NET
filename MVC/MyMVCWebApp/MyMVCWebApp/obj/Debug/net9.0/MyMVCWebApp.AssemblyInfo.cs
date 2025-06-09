2665810675b3faf2d8632f2014ae21a6dab68255
melsa <melsa@MUHMD-SABAGH-PC>
2025-06-09 04:58:14 +0300

    some modifications

--- MVC/MyMVCWebApp/MyMVCWebApp/obj/Debug/net9.0/MyMVCWebApp.AssemblyInfo.cs
+++ MVC/MyMVCWebApp/MyMVCWebApp/obj/Debug/net9.0/MyMVCWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMVCWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb974a0f35beee3391e22d552208cf036cccb09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c110cf96fe1305b7e1279ac2ec2f836aa8c7f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMVCWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMVCWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
