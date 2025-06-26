5cfdd055303bbce606f6f30e7f86196b3c8f571c
melsa <melsa@MUHMD-SABAGH-PC>
2025-06-27 00:45:40 +0300

    Day-05 tasks completed - Updates on tasks Day-03

--- Angular/Day-03/Component-Interaction/src/app/app.ts
+++ Angular/Day-03/Component-Interaction/src/app/app.ts
@@ -1,29 +1,20 @@
 import { Component } from '@angular/core';
-// Import all components that will be conditionally displayed
 import { Navbar } from './components/navbar/navbar';
 import { Home } from './components/home/home';
 import { Movies } from './components/movies/movies';
 import { CapitalizeInput } from './components/capitalize-input/capitalize-input';
-import { CommonModule } from '@angular/common'; // Import CommonModule for *ngIf
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  // Add all components to imports since they might be rendered
   imports: [Navbar, Home, Movies, CapitalizeInput, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   title = 'angular-merged-app';
-  // State variable to control which component is displayed
-  selectedView: string = 'home'; // Default to 'home' view
+  selectedView: string = 'home';
 
-  /**
-   * Handles the navigation event emitted by the NavbarComponent.
-   * Updates the selectedView to display the appropriate component.
-   * @param view The name of the view to switch to (e.g., 'home', 'movies', 'capitalize').
-   */
   onNavigate(view: string): void {
     this.selectedView = view;
   }
